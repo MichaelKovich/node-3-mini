9daac212238ce54b110fbd21c6540c4bc77cc50b
Michael Kovich <kovich@michaelkovich.com>
2018-03-13 17:34:13 -0500

    Project complete.

--- server/index.js
+++ server/index.js
@@ -1,16 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mc = require( `./controllers/messages_controller` );
+const mc = require(`./controllers/messages_controller`);
+const session = require('express-session');
+require('dotenv').config();
+
+const createInitialSession = require('./middleware/session');
+const filter = require('./middleware/filter');
 
 const app = express();
 
-app.use( bodyParser.json() );
-app.use( express.static( `${__dirname}/../build` ) );
+app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/../build`));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {maxAge: 10000}
+  })
+);
 
-app.post( "/api/messages", mc.create );
-app.get( "/api/messages", mc.read );
-app.put( "/api/messages", mc.update );
-app.delete( "/api/messages", mc.delete );
+app.use((req, res, next) => createInitialSession(req, res, next));
+app.use((req, res, next) => {
+  const {method} = req;
+  if (method === 'POST' || method === 'PUT') {
+    filter(req, res, next);
+  } else {
+    next();
+  }
+});
 
-const port = process.env.PORT || 3000
-app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
+app.get('/api/messages', mc.read);
+app.get('/api/messages/history', mc.history);
+app.post('/api/messages', mc.create);
+app.put('/api/messages', mc.update);
+app.delete('/api/messages', mc.delete);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}.`);
+});
